9f4cd867a288532af5022155afa836a917d014c8
Alisherb28 <alish.tussupbaev@gmail.com>
2023-04-04 00:54:08 -1000

    new project, setup tools

--- src/test/java/com/library/runners/CukesRunner.java
+++ src/test/java/com/library/runners/CukesRunner.java
@@ -15,7 +15,8 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/library/step_definitions",
         dryRun = false,
-        tags = "@smoke"
+        tags = "",
+        publish=true
 )
 public class CukesRunner {
 
