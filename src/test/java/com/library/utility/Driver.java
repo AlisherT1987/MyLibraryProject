f599e8732921b9a001222d16f2b517892351dd96
Alisherb28 <alish.tussupbaev@gmail.com>
2023-04-04 01:06:22 -1000

    new project, setup tools

--- src/test/java/com/library/utility/Driver.java
+++ src/test/java/com/library/utility/Driver.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.URL;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
@@ -24,68 +25,34 @@ public class Driver {
     //
     //private static WebDriver driver; // value is null by default
 
-    private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
+    private static final InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
 
 
     // Create a re-usable utility method which will return same driver instance when we call it
-    public static WebDriver getDriver() {
+    public static WebDriver getDriver(){
 
-        // it will check if driver is null and if it is we will set up browser inside if statement
-        // if you already setup driver and using it again for following line of codes, it will return to same driver
-        if (driverPool.get() == null) {
-            String browserName = System.getProperty("browser") != null ? browserName = System.getProperty("browser") : ConfigurationReader.getProperty("browser");
+        if (driverPool.get() == null){
 
+            String browserType = ConfigurationReader.getProperty("browser");
+            browserType=browserType.toLowerCase().trim();
 
-
-            switch(browserName){
-                case "remote-chrome":
-                    try {
-                        // assign your grid server address
-                        String gridAddress = "34.202.159.59";
-                        URL url = new URL("http://"+ gridAddress + ":4444/wd/hub");
-                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-                        desiredCapabilities.setBrowserName("chrome");
-                        driverPool.set(new RemoteWebDriver(url, desiredCapabilities));
-                        //driverPool.set(new RemoteWebDriver(new URL("http://0.0.0.0:4444/wd/hub"),desiredCapabilities));
-
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                case "remote-firefox":
-                    try {
-                        // assign your grid server address
-                        String gridAddress = "34.202.159.59";
-                        URL url = new URL("http://"+ gridAddress + ":4444/wd/hub");
-                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-                        desiredCapabilities.setBrowserName("firefox");
-                        driverPool.set(new RemoteWebDriver(url, desiredCapabilities));
-                        //driverPool.set(new RemoteWebDriver(new URL("http://0.0.0.0:4444/wd/hub"),desiredCapabilities));
-
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    break;
+            switch (browserType){
                 case "chrome":
-                    WebDriverManager.chromedriver().setup();
                     driverPool.set(new ChromeDriver());
                     driverPool.get().manage().window().maximize();
-                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
                     break;
+
                 case "firefox":
-                    WebDriverManager.firefoxdriver().setup();
                     driverPool.set(new FirefoxDriver());
                     driverPool.get().manage().window().maximize();
-                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
                     break;
+
             }
-
         }
-
         return driverPool.get();
-
     }
-
     // This method will make sure our driver value is always null after using quit() method
     public static void closeDriver(){
         if(driverPool.get() != null){
